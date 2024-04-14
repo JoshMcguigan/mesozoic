4a176651c1d47bee469ac4cc37ca327a570007c9
Josh Mcguigan <joshmcg88@gmail.com>
2024-04-14 11:42:50 -0700

    remove unused BLE battery service value - currently unused

--- platform/src/ble.rs
+++ platform/src/ble.rs
@@ -139,7 +139,6 @@ impl SecurityHandler for Bonder {
 }
 
 pub struct BatteryService {
-    value_handle: u16,
     cccd_handle: u16,
 }
 
@@ -156,31 +155,8 @@ impl BatteryService {
         let _service_handle = service_builder.build();
 
         Ok(BatteryService {
-            value_handle: characteristic_handles.value_handle,
             cccd_handle: characteristic_handles.cccd_handle,
         })
-    }
-
-    pub fn battery_level_get(&self, sd: &Softdevice) -> Result<u8, gatt_server::GetValueError> {
-        let buf = &mut [0u8];
-        gatt_server::get_value(sd, self.value_handle, buf)?;
-        Ok(buf[0])
-    }
-
-    pub fn battery_level_set(
-        &self,
-        sd: &Softdevice,
-        val: u8,
-    ) -> Result<(), gatt_server::SetValueError> {
-        gatt_server::set_value(sd, self.value_handle, &[val])
-    }
-
-    pub fn battery_level_notify(
-        &self,
-        conn: &Connection,
-        val: u8,
-    ) -> Result<(), gatt_server::NotifyValueError> {
-        gatt_server::notify_value(conn, self.value_handle, &[val])
     }
 
     pub fn on_write(&self, handle: u16, data: &[u8]) {
